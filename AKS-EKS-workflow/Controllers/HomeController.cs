c798f699cf4774e24b1bef8a096247ac4acc2089
Sai Sreeprada T <sreeprada632003@gmail.com>
2024-12-19 14:51:49 +0530

    Add project files.

---
+++ AKS-EKS-workflow/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using AKS_EKS_workflow.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AKS_EKS_workflow.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AKS-EKS-workflow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AKS_EKS_workflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
